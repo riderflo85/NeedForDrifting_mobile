41d72ab43d8754a9237871d7a424440405a1efca
Florent (Mac) <grenaille.florent85@gmail.com>
2020-08-23 17:38:02 +0200

    Create first component named login page

--- components/login.js
+++ components/login.js
@@ -1,25 +1,70 @@
 import React from 'react';
-import { View, TextInput, Text } from 'react-native';
+import { View, TextInput, Text, Image, Switch, TouchableOpacity } from 'react-native';
 import { loginStyle } from '../static/styles';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 
 class Login extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            stayConnected: true
+        }
+    }
+
+    _userStayConnected = (value) => {
+        this.setState({
+            stayConnected: value
+        });
+    }
+
+    _loginUser() {
+        console.log('user login');
     }
 
     render() {
         return (
             <View style={loginStyle.main}>
+                <View style={loginStyle.boxLogo}>
+                    <Image style={loginStyle.logo} source={require('../static/images/logo.png')}/>
+                    <Image style={loginStyle.logoP2} source={require('../static/images/logo_part2-2.png')}/>
+                </View>
                 <View style={loginStyle.inputBox}>
-                    <Text>Nom d'utilisateur</Text>
+                    <Text style={loginStyle.welcome}>Bienvenue !</Text>
+                    <Text style={loginStyle.titleInput} >Nom d'utilisateur</Text>
                     <TextInput style={loginStyle.inputArea}/>
-                    <Text>Mot de passe</Text>
-                    <TextInput style={loginStyle.inputArea} autoCapitalize='none' secureTextEntry={true} textContentType='password'/>
-                    <Text>Clé API</Text>
-                    <TextInput style={loginStyle.inputArea} autoCapitalize='none'/>
+                    <Text style={loginStyle.titleInput} >Mot de passe</Text>
+                    <TextInput
+                        style={loginStyle.inputArea}
+                        autoCapitalize='none'
+                        secureTextEntry={true}
+                        textContentType='password'
+                    />
+                    <Text style={loginStyle.titleInput} >Clé API</Text>
+                    <TextInput
+                        style={loginStyle.inputArea}
+                        autoCapitalize='none'
+                    />
+                    <View style={loginStyle.stayConnected}>
+                        <Text>Rester connecté ? </Text>
+                        <Switch
+                            trackColor={{ false: "#767577", true: "#0d96d1" }}
+                            onValueChange={this._userStayConnected}
+                            value={this.state.stayConnected}
+                        />
+                    </View>
+                    <TouchableOpacity
+                        style={loginStyle.buttonLogin}
+                        onPress={this._loginUser}
+                    >
+                        <View style={loginStyle.blocButtonLogin}>
+                            <Text style={loginStyle.textButtonLogin}>CONNEXION</Text>
+                            <MaterialCommunityIcons name="arrow-right-drop-circle-outline" size={25} color="white"/>
+                        </View>
+                    </TouchableOpacity>
                 </View>
             </View>
+            
         );
     }
 }
--- static/styles.js
+++ static/styles.js
@@ -15,11 +15,39 @@ export default test;
 export const loginStyle = StyleSheet.create({
     main: {
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
+        paddingTop: 30,
+        backgroundColor: 'rgb(228,228,228)'
+        // alignItems: "center",
+        // justifyContent: "center",
+    },
+    boxLogo: {
+        flex: 0.6,
+        alignItems: "center"
+    },
+    logo: {
+        marginTop: 20,
+        marginBottom: 20,
     },
     inputBox: {
-        width: '50%',
+        flex: 1,
+        // width: '50%',
+        justifyContent: "center",
+        marginTop: 50,
+        backgroundColor: 'white',
+        paddingBottom: 15,
+        paddingHorizontal: 40,
+        borderTopStartRadius: 35,
+        borderTopEndRadius: 35
+    },
+    welcome: {
+        marginTop: 15,
+        marginBottom: 30,
+        fontSize: 30,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    titleInput: {
+        color: '#737373',
     },
     inputArea: {
         width: '100%',
@@ -27,5 +55,28 @@ export const loginStyle = StyleSheet.create({
         marginBottom: 25,
         borderBottomWidth: 1,
         borderColor: 'grey'
-    }
+    },
+    stayConnected: {
+        flexDirection: 'row',
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginBottom: 20,
+    },
+    buttonLogin: {
+        alignSelf: 'center',
+        width: '70%',
+        backgroundColor: '#0d96d1',
+        padding: 13,
+        borderRadius: 30,
+    },
+    blocButtonLogin: {
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+    textButtonLogin: {
+        color: 'white',
+        textAlign: "center",
+        fontSize: 20,
+        fontWeight: '500',
+    },
 });

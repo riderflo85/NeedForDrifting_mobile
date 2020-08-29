8b700a8d49349aa2548b20870a2cb9405b409863
Florent (Mac) <grenaille.florent85@gmail.com>
2020-08-29 18:32:34 +0200

    Add the news functions for call API

--- api/NFD_api.js
+++ api/NFD_api.js
@@ -13,3 +13,27 @@ export function getAllServers(url, username, token) {
     .then((response) => response.json())
     .catch((error) => console.log(error));
 }
+
+export function getTracks(url, username, token) {
+    const newUrl = `${url}/api/v1/get_tracks?username=${username}&api=${token}`;
+
+    return fetch(newUrl)
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+}
+
+export function changeTrack(url, username, token, idServer,idTrack, configTrack, maxClients) {
+    const newUrl = `${url}/api/v1/change_track?username${username}&api=${token}&server_id=${idServer}&track_id=${idTrack}&config_track=${configTrack}&max_clients=${maxClients}`;
+
+    return fetch(newUrl)
+    .then((response) => response.json())
+    .catch((error) => console.log(error))
+}
+
+export function runCommand(url, username, token, idServer, cmd) {
+    const newUrl = `${url}/api/v1/run_command?username=${username}&api=${token}&server_id=${idServer}&server_cmd=${cmd}`;
+
+    return fetch(newUrl)
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+}
